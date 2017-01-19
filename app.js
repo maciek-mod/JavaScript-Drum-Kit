f468b2b5706fe72bf5329192e6cd15b2ccdbfb6b
Krzysztof Lubiniecki <klubiniecki@gmail.com>
2017-01-19 16:31:45 +0100

    Minor fix to new version

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 class Drumkit {
-	constructor() {
+  constructor() {
 
-		this.playSound = this.playSound.bind(this);
+    this.playSound = this.playSound.bind(this);
+    this.transitionEnd = this.transitionEnd.bind(this);
 
     document.body.addEventListener('keydown', this.playSound);
-		document.body.addEventListener('transitionend', this.transitionEnd);
-	}
+    document.body.addEventListener('transitionend', this.transitionEnd);
+  }
 
-	playSound(e) {
-		const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
 
     key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
-	}
+  }
 
-	transitionEnd(e) {
-		if (e.target.classList.contains('key')) {
-			e.target.classList.remove('playing');
-		}
-	}
+  transitionEnd(e) {
+    if (e.target.classList.contains('key')) {
+      e.target.classList.remove('playing');
+    }
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	const drumkit = new Drumkit();
+  const drumkit = new Drumkit();
+});
